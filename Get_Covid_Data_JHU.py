01f86e049bf7313f0411fa4eeb699f53f1e2c4c7
MWelHeb <62515375+MWelHeb@users.noreply.github.com>
2020-09-18 14:33:03 +0200

    Add files via upload

--- Get_Covid_Data_JHU.py
+++ Get_Covid_Data_JHU.py
@@ -7,6 +7,9 @@ Created on Thu Sep  3 15:54:26 2020
 
 import pandas as pd
 
+
+#####################################################################################
+# (1) get covid data from JHU and cleanse some Country Names
 url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 confirmed = pd.read_csv(url, error_bad_lines=False)
 print(confirmed)
